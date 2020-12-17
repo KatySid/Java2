46d27c9ccd18623bbe55ee2be8d162816ba243c3
KatySid <whitekaty666@gmail.com>
2020-12-17 21:28:41 +0300

    Initial commit

---
+++ src/Lesson1/Person.java
@@ -0,0 +1,37 @@
+package Lesson1;
+
+public class Person implements Trafficable {
+    private String name;
+    private double distance;
+    private double height;
+
+    public Person (String name, double distance, double height){
+        this.name = name;
+        this.distance = distance;
+        this.height = height;
+
+    }
+
+    @Override
+    public boolean run (double s) {
+        if (s <= distance) {
+            System.out.println(name + " ran " + s + "meters");
+            return  true;
+        } else {
+            System.out.println(name + "can not run");
+            return false;
+        }
+    }
+
+    @Override
+    public boolean jump(double h) {
+        if (h<=height){
+            System.out.println(name + " jump " + h + " meters");
+            return true;
+        } else {
+            System.out.println(name + "can not jump");
+            return false;
+        }
+
+    }
+}
